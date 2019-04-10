ef8e0ea45e927de00e7714325b021f69c5c27602
br7373 <46458606+br7373@users.noreply.github.com>
2019-04-09 22:12:29 -0500

    Add files via upload

---
+++ Number Frequency/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPress = New System.Windows.Forms.Button()
+        Me.lstFreq = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnPress
+        '
+        Me.btnPress.Location = New System.Drawing.Point(153, 73)
+        Me.btnPress.Name = "btnPress"
+        Me.btnPress.Size = New System.Drawing.Size(143, 36)
+        Me.btnPress.TabIndex = 0
+        Me.btnPress.Text = "Press"
+        Me.btnPress.UseVisualStyleBackColor = True
+        '
+        'lstFreq
+        '
+        Me.lstFreq.FormattingEnabled = True
+        Me.lstFreq.Location = New System.Drawing.Point(153, 115)
+        Me.lstFreq.Name = "lstFreq"
+        Me.lstFreq.Size = New System.Drawing.Size(143, 95)
+        Me.lstFreq.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(458, 303)
+        Me.Controls.Add(Me.lstFreq)
+        Me.Controls.Add(Me.btnPress)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnPress As Button
+    Friend WithEvents lstFreq As ListBox
+End Class
---
+++ Number Frequency/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    Private Sub btnPress_Click(sender As Object, e As EventArgs) Handles btnPress.Click
+        Dim warray() As Integer = {4, 3, 2, 1, 4, 4, 3, 2, 1, 6, 7, 8, 8, 8, 7, 7, 5, 5, 5, 0}
+        Dim zero As Byte
+        Dim one As Byte
+        Dim two As Byte
+        Dim three As Byte
+        Dim four As Byte
+        Dim five As Byte
+        Dim six As Byte
+        Dim seven As Byte
+        Dim eight As Byte
+
+        Array.Sort(warray)
+
+        lstFreq.Items.Add("Digit" & vbTab & "Frequency")
+
+        For i As Byte = 0 To 19
+            If warray(i) = 0 Then
+                zero += 1
+            ElseIf warray(i) = 1 Then
+                one += 1
+            ElseIf warray(i) = 2 Then
+                two += 1
+            ElseIf warray(i) = 3 Then
+                three += 1
+            ElseIf warray(i) = 4 Then
+                four += 1
+            ElseIf warray(i) = 5 Then
+                five += 1
+            ElseIf warray(i) = 6 Then
+                six += 1
+            ElseIf warray(i) = 7 Then
+                seven += 1
+            ElseIf warray(i) = 8 Then
+                eight += 1
+            End If
+        Next
+
+        lstFreq.Items.Add("0" & vbTab & zero)
+        lstFreq.Items.Add("1" & vbTab & one)
+        lstFreq.Items.Add("2" & vbTab & two)
+        lstFreq.Items.Add("3" & vbTab & three)
+        lstFreq.Items.Add("4" & vbTab & four)
+        lstFreq.Items.Add("5" & vbTab & five)
+        lstFreq.Items.Add("6" & vbTab & six)
+        lstFreq.Items.Add("7" & vbTab & seven)
+        lstFreq.Items.Add("8" & vbTab & eight)
+    End Sub
+End Class
